1c935aca68ba1cfb81b0cd905453bf1bf9b29b5a
TrueLightbringer <33877015+TrueLightbringer@users.noreply.github.com>
2017-11-27 14:54:08 +0200

    Update Task_3.java

--- src/com/part_2/Task_3.java
+++ src/com/part_2/Task_3.java
@@ -7,7 +7,7 @@ public class Task_3 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введіть рік");
         int year = sc.nextInt();
-        if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0){
+        if (year % 4 == 0 && year % 100 == 0 || year % 400 == 0){
             System.out.println("Рік високосний");
         }else {
             System.out.println("Рік не високосний");
